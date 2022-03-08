adf28aba1aff191f6084694fe0168d584383049a
juanfed <juanfed12345@gmail.com>
2022-03-08 08:52:32 -0500

    cambios menores en el css en el header y login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,6 @@ const Header = () => {
     return (
         <header className='header'>
             <h1 className="header--title">Yu-Gi-Oh!</h1>
-            {/* <ul className='header--li'>
-                <Link to="/"><li>Inicio</li></Link>
-                <Link to="/pagina1"><li>Pagina 1</li></Link>
-                <Link to="/pagina2"><li>Pagina 2</li></Link>
-                <Link to="/pagina3"><li>Pagina 3</li></Link>
-                <Link to="/pagina4"><li>Pagina 4</li></Link>
-            </ul> */}
         </header>
     )
 }
