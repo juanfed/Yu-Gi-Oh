0b8b9b4868195a8f79485ac450ac901463565284
juanfed <juanfed12345@gmail.com>
2022-03-08 08:26:16 -0500

    solucion de problema con la ruta de inicio

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom" // version 6
 import App from './App';
 import Inicio from './pages/Inicio';
-import Pagina1 from './pages/Pagina1';
-import Pagina2 from './pages/Pagina2';
-import Pagina3 from './pages/Pagina3';
-import Pagina4 from './pages/Pagina4';
 
 import NoFound from './components/NoFound';
 
@@ -15,11 +11,7 @@ ReactDOM.render(
   <Router>
     <Routes>
       <Route path='/' element={<App />} >
-        <Route path='/' element={<Inicio />}/>
-        <Route path='/pagina1' element={<Pagina1 />}/>
-        <Route path='/pagina2' element={<Pagina2 />}/>
-        <Route path='/pagina3' element={<Pagina3 />}/>
-        <Route path='/pagina4' element={<Pagina4 />}/>
+        <Route path='/Yu-Gi-Oh' element={<Inicio />}/>
       </Route>
       <Route path='*' element={<NoFound />} />
     </Routes>
