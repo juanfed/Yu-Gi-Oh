6657daa1daaf322f91b6fd5c4ab00eb94308ab08
juanfed <juanfed12345@gmail.com>
2022-03-09 17:15:18 -0500

    agregue un archivo el cual contiene el contenido que se irá a mostrar una vez la persona se haya logueado correctamente

--- src/components/HeaderPrivate.jsx
+++ src/components/HeaderPrivate.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import '../styles/header.css';
+
+import '../styles/headerPrivate.css';
 
 const HeaderPrivate = () => {
     return (
         <header className='header'>
             <h1 className="header--title">Yu-Gi-Oh! Private</h1>
+            <div className='header--buton'>
+            <button className='header--buton--buton'>Sing Out</button>
+            </div>
         </header>
     )
 }
--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+// import fondoCards from '../assets/img7.jpg';
 
 const Cards = () => {
     return (
         <main>
-            <h1>Pagina de las cartas randon</h1>
+            <section className='mazo'>
+                <h3>Generar carta aleatoria</h3>
+
+                <div className='mazo__cards'>
+                    <div className='mazo__cards--img'>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </section>
+
+            <section>
+
+            </section>
+
+            
         </main>
     )
 }
