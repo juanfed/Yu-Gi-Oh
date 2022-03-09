1e1ff94b01fae371b4c48b033b3d1d7fad71c7f4
juanfed <juanfed12345@gmail.com>
2022-03-09 01:06:03 -0500

    Autenticacion con google funcional

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../styles/login.css';
-
 import google from '../assets/loginGoogle.png';
 
+
+//trabajo con el login usando la autenticacion de google
+import GoogleLogin from 'react-google-login';
+
 const Login = () => {
+	const responseGoogle = (sucsess) => {
+		console.log(sucsess);
+		console.log(sucsess.profileObj);
+
+	}
 	return (
 		<div className='login'>
 			<h4 className='login--title'>User Login</h4>
@@ -19,9 +27,24 @@ const Login = () => {
 				<button className='login--buton'>Sing In</button>
 			</div>
 			<div className='google'>
-				<figure className='google--figure'>
-					<img src={google} alt="Login whit google" />
-				</figure>
+				<GoogleLogin
+					clientId="39874577063-ujhsadl9bdtnhta5m1ndv1hrppjn706n.apps.googleusercontent.com"
+					// buttonText="Sign up with Google"
+					render={renderProps => (
+						<div>
+							<figure className='google--figure'>
+								<img 
+								disabled={renderProps.disabled}
+								onClick={renderProps.onClick}
+								src={google} alt="Login whit google" />
+							</figure>
+						</div>
+
+					)}
+					onSuccess={responseGoogle}
+					onFailure={responseGoogle}
+					cookiePolicy={'single_host_origin'}
+				/>
 			</div>
 			<div className='register'>
 				<p>Don't have an account?</p>
