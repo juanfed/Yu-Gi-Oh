815915a6e0f4e3edf30bb4d3e8f3e774678d94a6
juanfed <juanfed12345@gmail.com>
2022-03-10 09:33:11 -0500

    empeze a trabajar con la api de las cartas

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../styles/cards.css';
 // import fondoCards from '../assets/img7.jpg';
 
 const Cards = () => {
-    return (
-        <main>
-            <section className='mazo'>
-                <h3>Generar carta aleatoria</h3>
+	let array = [];
+	// cartas aleatorias
+for(let i = 0; i < 8; i++){
+	let numero = Math.round(Math.random()*9000);
+	array.push(numero)
+}
+console.log(array);
 
-                <div className='mazo__cards'>
-                    <div className='mazo__cards--img'>
-                        <img src="" alt="" />
-                    </div>
-                </div>
-            </section>
+	const [resultado, setResultado] = useState('');
+	useEffect(() => {
+		const traerCarta = async () => {
+			const result = await axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php`)
+			setResultado(result);
+			console.log(resultado.data.data[400].name);
+			console.log(resultado.data);
+		}
 
-            <section>
+		traerCarta()
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+	return (
+		<main>
+			<section className='mazo'>
+				<h3>Generar carta aleatoria</h3>
 
-            </section>
+				<div className='mazo__cards'>
+					<div className='mazo__cards--img'>
+					</div>
+				</div>
+			</section>
 
-            
-        </main>
-    )
+			<section>
+
+			<div className='card'>
+					<h3>{resultado.data.data[999].name}</h3>
+					<figure className='card--figure'>
+						<img src={resultado.data.data[999].card_images[0].image_url} alt={resultado.data.data[999].name} />
+					</figure>
+				</div>
+
+
+
+			</section>
+
+
+		</main>
+	)
 }
 
 export default Cards
