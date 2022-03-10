0864560e343987a8bbbc63cfe6f022541fd44d40
juanfed <juanfed12345@gmail.com>
2022-03-10 10:37:57 -0500

    consumo de api y mostrando cards

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -6,46 +6,38 @@ import '../styles/cards.css';
 const Cards = () => {
 	let array = [];
 	// cartas aleatorias
-for(let i = 0; i < 8; i++){
-	let numero = Math.round(Math.random()*9000);
-	array.push(numero)
-}
-console.log(array);
+	for (let i = 0; i < 8; i++) {
+		let numero = Math.round(Math.random() * 9000);
+		array.push(numero)
+	}
+	console.log(array);
 
 	const [resultado, setResultado] = useState('');
 	useEffect(() => {
 		const traerCarta = async () => {
 			const result = await axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php`)
 			setResultado(result);
-			console.log(resultado.data.data[400].name);
-			console.log(resultado.data);
 		}
 
 		traerCarta()
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 	return (
-		<main>
-			<section className='mazo'>
-				<h3>Generar carta aleatoria</h3>
+		<main className='main--cards'>
+			<h3>Mis cartas</h3>
 
-				<div className='mazo__cards'>
-					<div className='mazo__cards--img'>
+
+			<section className='cards'>
+				{array.map((card) => (
+					<div className='card'
+					key={card}>
+						<figure className='card--figure'>
+							<img src={resultado.data?.data[card].card_images[0].image_url}
+								
+								alt={resultado.data?.data[card].name} />
+						</figure>
 					</div>
-				</div>
-			</section>
-
-			<section>
-
-			<div className='card'>
-					<h3>{resultado.data.data[999].name}</h3>
-					<figure className='card--figure'>
-						<img src={resultado.data.data[999].card_images[0].image_url} alt={resultado.data.data[999].name} />
-					</figure>
-				</div>
-
-
-
+				))}
 			</section>
 
 
