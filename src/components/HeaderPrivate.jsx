b5896cc17945805a7faa1c473bf3e75aec9f1279
juanfed <juanfed12345@gmail.com>
2022-03-10 22:11:13 -0500

    trabajo en la card aleatorio y mostrar la card seleccionada al lado con un mayor tamaño

--- src/components/HeaderPrivate.jsx
+++ src/components/HeaderPrivate.jsx
@@ -6,7 +6,7 @@ import '../styles/headerPrivate.css';
 const HeaderPrivate = () => {
     return (
         <header className='header'>
-            <h1 className="header--title">Yu-Gi-Oh! Private</h1>
+            <h1 className="header--title">Yu-Gi-Oh!</h1>
             <div className='header--buton'>
             <button className='header--buton--buton'>Sing Out</button>
             </div>
--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 import '../styles/cards.css';
 // import fondoCards from '../assets/img7.jpg';
 
-const Cards = () => {
-	let array = [];
+function Cards(){
+	const [datoCard, setDatoCard] = useState([999]);
 	// cartas aleatorias
-	for (let i = 0; i < 8; i++) {
-		let numero = Math.round(Math.random() * 9000);
-		array.push(numero)
-	}
+	const [array, setArray] = useState([])
 	console.log(array);
-
+	
 	const [resultado, setResultado] = useState('');
 	useEffect(() => {
+		let arrays = [];
+		for (let i = 0; i < 8; i++) {
+			let numero = Math.round(Math.random() * 9000);
+			arrays.push(numero)
+		}
+		setArray(arrays);
 		const traerCarta = async () => {
 			const result = await axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php`)
 			setResultado(result);
@@ -22,27 +25,53 @@ const Cards = () => {
 		traerCarta()
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
+
+	const mostrarDatos = (card) => {
+		setDatoCard(card)
+		console.log(datoCard);
+	}
+
 	return (
 		<main className='main--cards'>
-			<h3>Mis cartas</h3>
+			<div className='main--cards--buton'>
+				<button>Generar cartas aleatorias</button>
+			</div>
 
+			<div className='contenedor--cards'>
+				<section className='cards'>
+					{array.map((card) => (
+						<div className='card'
+							onClick={() => { mostrarDatos(card) }}
+							key={card}>
+							<figure className='card--figure'>
+								<img src={resultado.data?.data[card].card_images[0].image_url}
+									className='card--figure--img'
+									alt={resultado.data?.data[card].name} />
+							</figure>
+						</div>
+					))}
+				</section>
 
-			<section className='cards'>
-				{array.map((card) => (
-					<div className='card'
-					key={card}>
-						<figure className='card--figure'>
-							<img src={resultado.data?.data[card].card_images[0].image_url}
-								
-								alt={resultado.data?.data[card].name} />
+				<section className='select--card'>
+					<div className='select--card--div'>
+						<figure className='select--card--figure'>
+							<img src={resultado.data?.data[datoCard].card_images[0].image_url}
+								className='select--card--figure--img'
+								alt={resultado.data?.data[datoCard].name} />
 						</figure>
+						<button onClick={mostrarDatos}>Agregar</button>
 					</div>
-				))}
-			</section>
+
+				</section>
+
+				<br /><br /><br /><br />
+
+			</div>
 
 
 		</main>
 	)
 }
 
-export default Cards
+
+export default React.memo(Cards)
