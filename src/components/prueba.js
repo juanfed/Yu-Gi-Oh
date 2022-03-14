17ae89fdf97580b9657a66aafdf5c635ad4dbd77
juanfed <juanfed12345@gmail.com>
2022-03-14 18:42:14 -0500

    trabajando en la pagina de mis cartas

--- src/components/prueba.js
+++ src/components/prueba.js
@@ -1,2 +1,5 @@
-let dato = [];
-console.log(dato);
+let mostrarCata = [false,false,false,false,false,false,false,false,false];
+
+mostrarCata[2] = true;
+console.log(mostrarCata[2])
+console.log(mostrarCata[5])
--- src/pages/MyCards.jsx
+++ src/pages/MyCards.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../styles/myCards.css';
+import cartaReverse from '../images/reverseCard.jpg';
+
 
 const MyCards = () => {
-    return (
-        <div>MyCards</div>
-    )
+	const mostrarCarta = [false, false, false, false, false, false, false, false, false];
+	const cartas = [1, 2, 3, 4, 5, 6, 7];
+	const [resultado, setResultado] = useState([]);
+
+	useEffect(() => {
+		const traerCarta = async () => {
+			const result = await axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php`)
+			setResultado(result);
+		}
+		traerCarta()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+	const mostrar = (carta) => {
+		if (mostrarCarta[carta]) {
+			mostrarCarta[carta] = false
+		} else {
+			mostrarCarta[carta] = true
+		}
+
+		console.log(mostrarCarta);
+	}
+
+	return (
+		<main className='mycards'>
+			<h2 className='mycards--title'>Mi mazo personalizado</h2>
+			<section className='cartas'>
+				{cartas.map((carta) => (
+					<div className={`carta${carta} carta`} onClick={() => { mostrar(carta) }}
+						key={carta}>
+						{mostrarCarta[carta] ? <img src={resultado.data?.data[carta].card_images[0].image_url} alt="carta 1" /> :
+							<img src={cartaReverse} alt="carta 1" />  }
+					</div>
+				))}
+			</section>
+		</main>
+	)
 }
 
 export default MyCards
